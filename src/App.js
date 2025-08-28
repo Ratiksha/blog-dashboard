94405a25d5768381a34bded6f46f801d3be427ac
Ratiksha <ratikshadeikar@gmail.com>
2025-08-28 14:37:53 -0700

    Minor Changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import React, { useState } from "react";
+import React, { useState, Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { getTheme } from "./theme";
 import Navbar from "./components/Navbar";
 
-const Dashboard = React.lazy(() => import('./pages/Dashboard'))
-const ViewBlogPage = React.lazy(() => import('./components/ViewBlogPage'))
+const Dashboard = lazy(() => import('./pages/Dashboard'))
+const ViewBlogPage = lazy(() => import('./components/ViewBlogPage'))
 
 function App() {
   const [mode, setMode] = useState("light");
@@ -18,10 +18,12 @@ function App() {
       <CssBaseline />
       <Router>
         <Navbar mode={mode} toggleMode={toggleMode} />
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/posts/:id" element={<ViewBlogPage />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/posts/:id" element={<ViewBlogPage />} />
+          </Routes>
+        </Suspense>
       </Router>
     </ThemeProvider>
   );
